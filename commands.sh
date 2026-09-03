bf61d4d601887b9bc08bdd6f123fb66174a109e8
Ryan Kirsch <agalloch88@gmail.com>
2026-09-03 06:00:48 -0400

    feat: update commands

--- commands.sh
+++ commands.sh
@@ -1,24 +1,21 @@
 ## command to create a repository in AWS ECR With name calendly
 
-aws ecr create-repository --repository-name calendly
+aws ecr create-repository --repository-name calendly-cicd-codebuild
 
 ## command  to buld the Docker Container Image (dont forget the dot at the end)
-docker build -t calendly .
+docker build -t calendly-cicd-codebuild .
 
-docker build --platform linux/amd64 -t calendly .
+docker build --platform linux/amd64 -t calendly-cicd-codebuild .
 
 
 ## Command to give the tag Lastest to the Docker Image
 
-docker tag calendly:latest 980921724429.dkr.ecr.us-east-1.amazonaws.com/calendly:latest
+docker tag calendly-cicd-codebuild:latest 126304775778.dkr.ecr.us-east-1.amazonaws.com/calendly-cicd-codebuild:latest
 
 ##you can get above URI from AWS ECR console
 ## Command to connect to AWS ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 980921724429.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 126304775778.dkr.ecr.us-east-1.amazonaws.com
 
-## replace this 980921724429.dkr.ecr.us-east-1.amazonaws.com with your URI from AWS ECR console
 ## Command to Push the latest Image to AWS ECR
 
-docker push 980921724429.dkr.ecr.us-east-1.amazonaws.com/calendly:latest
-
-
+docker push 126304775778.dkr.ecr.us-east-1.amazonaws.com/calendly-cicd-codebuild:latest
