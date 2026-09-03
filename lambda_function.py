84a21d17eb14982c6b0f01258011ebcb1382fe02
Ryan Kirsch <agalloch88@gmail.com>
2026-09-03 05:56:30 -0400

    feat: update s3 details

--- lambda_function.py
+++ lambda_function.py
@@ -12,8 +12,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # Environment Variables (Configured in Lambda)
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME', 'pvh-kareem')
-S3_FOLDER_PATH = os.getenv('S3_FOLDER_PATH', 'calendly/')
+S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME', 'dea-ryan')
+S3_FOLDER_PATH = os.getenv('S3_FOLDER_PATH', 'calendly-cicd/')
 SECRET_NAME = os.getenv('CALENDLY_SECRET_NAME', 'calendly-api-key')
 REGION_NAME = os.getenv('AWS_REGION', 'us-east-1')
 
